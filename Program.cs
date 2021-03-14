9bb87cdd9138ffb465e8635598132f275d324417
Kaylany Paulus <kaylanysilvapaulus@gmail.com>
2021-03-14 16:50:22 -0300

    Acrescentando writeline

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hello_World_by_Kaylany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Oioi Professores, tenham uma ótima noite!"); 
+               }
+    }
+}
